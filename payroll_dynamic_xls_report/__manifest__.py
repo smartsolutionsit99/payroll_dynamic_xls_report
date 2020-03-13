88f07d4d775be4ed46526b706a3281970af74f41
root <you@example.com>
2020-03-13 08:17:17 -0700

    master

--- payroll_dynamic_xls_report/__manifest__.py
+++ payroll_dynamic_xls_report/__manifest__.py
@@ -16,7 +16,7 @@
     'auto_install': False,
     'installable': True,
     'application': True,
-    'licence': 'AGPL-3',
+    #'licence': 'GPL-3',
     'price': 25,
     'currency': 'USD',
     'support': 'smartsolutionsit99@gmail.com',
